10e089151c336c3285caaf70c9b917bf489b2ef0
Liz <lizi1898@gmail.com>
2019-08-23 00:18:07 -0500

    Corregido

--- models/user.js
+++ models/user.js
@@ -3,6 +3,8 @@ const { Schema } = mongoose;
 const bcrypt = require('bcrypt-nodejs');
 
 const UsersSchema  = new Schema({
+    fisrt_name: { type: String, require: true },
+    last_name: { type: String, require: true },
     email: { type: String, require: true },
     password: { type: String, require: true }
 });
